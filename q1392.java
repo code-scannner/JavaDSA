fe5de938dafcf075b8fae56626830b19cb282337
code-scanner <cyber.gupta001@gmail.com>
2023-11-15 15:01:32 +0530

    q1392 leetcode

---
+++ q1392.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class q1392{
+
+    public static String longestPrefix(String s) {
+        
+        return "";
+    }
+    public static void main(String[] args) {
+        String str = "ababab";
+
+        String pre = longestPrefix(str);
+
+        System.out.println(pre);
+
+    }
+}
