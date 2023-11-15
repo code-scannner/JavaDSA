3cf1b746e84c4bfe4a4537910812e8e67ef3e369
code-scanner <cyber.gupta001@gmail.com>
2023-11-15 15:01:13 +0530

    count subseqence

---
+++ countSubsequence.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class countSubsequence {
+    public static int count(String str){
+        int n = str.length();
+        int dp[] = new int[n + 1];
+        int idx[] = new int[26];
+        Arrays.fill(idx, -1);
+        dp[0] = 1;
+        for(int i = 0; i<n; i++){
+            char c = str.charAt(i);
+            dp[i + 1] = dp[i] * 2;
+            if(idx[c - 'a'] != -1){
+                dp[i + 1] -= dp[idx[c - 'a']];
+            }
+            idx[c - 'a'] = i;
+        }
+        
+        return dp[n];
+        
+    }
+
+    public static void main(String[] args) {
+        String str = "gfgg";
+        System.out.println("Total subsequences in "  + str + " = " + count(str));
+    }
+}
