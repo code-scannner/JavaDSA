5ba20b45b55c3c976366d62c034febc77842da2b
code-scanner <cyber.gupta001@gmail.com>
2024-01-07 01:12:14 +0530

    leetcode 1235

---
+++ leetcode/leet_1235.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.*;
+
+class Solution {
+    public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
+
+        int[][] multidim = new int[startTime.length][3];
+        for (int i = 0; i < multidim.length; i++) {
+            multidim[i][0] = startTime[i];
+            multidim[i][1] = endTime[i];
+            multidim[i][2] = profit[i];
+        }
+
+        Arrays.sort(multidim, (as, bs) -> as[0] - bs[0]);
+
+        int n = multidim[multidim.length - 1][0];
+
+        int dp[] = new int[n + 2];
+        int i = n, j = multidim.length - 1;
+        while (j >= 0) {
+            if (i == multidim[j][0]) {
+                int max = 0, interval = multidim[j][0];
+                while (j >= 0 && multidim[j][0] == interval) {
+                    max = Math.max(max, multidim[j][2] + (multidim[j][1] <= n ? dp[multidim[j][1]] : 0));
+                    j--;
+                }
+                dp[interval] = Math.max(max, dp[interval + 1]);
+            } else {
+                dp[i] = dp[i + 1];
+            }
+            i--;
+        }
+
+        return dp[i + 1];
+    }
+}
+
+public class leet_1235 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        // int[] startTime = { 1, 2, 3, 3 }, endTime = { 3, 4, 5, 6 }, profit = { 50,
+        // 10, 40, 70 };
+        int[] startTime = { 1, 2, 3, 4, 6 }, endTime = { 3, 5, 10, 6, 9 }, profit = { 20, 20, 100, 70, 60 };
+        int result = sol.jobScheduling(startTime, endTime, profit);
+        System.out.println(result);
+    }
+}
--- utilities/arrays.java
+++ utilities/arrays.java
@@ -4,12 +4,27 @@ import java.util.Arrays;
 
 public class arrays {
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-        int copy[] = Arrays.copyOf(arr, arr.length + 1);
-        System.out.println(Arrays.toString(copy));
-        System.out.println(Arrays.stream(arr).max().getAsInt());
-        System.out.println(Arrays.stream(copy).min().getAsInt());
-        System.out.println( Arrays.binarySearch(arr, 2));
+        // int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+        // int copy[] = Arrays.copyOf(arr, arr.length + 1);
+        // System.out.println(Arrays.toString(copy));
+        // System.out.println(Arrays.stream(arr).max().getAsInt());
+        // System.out.println(Arrays.stream(copy).min().getAsInt());
+        // System.out.println( Arrays.binarySearch(arr, 2));
+
+        // sorting arrays parrallely
+        int[] a = { 4, 2, 4, 8, 2 },
+                b = { 5, 5, 5, 10, 8 },
+                c = { 1,2,8,10,4};
+        int[][] multidim = new int[a.length][3];
+        for (int i = 0; i < multidim.length; i++) {
+            multidim[i][0] = a[i];
+            multidim[i][1] = b[i];
+            multidim[i][2] = c[i];
+        }
+
+        Arrays.sort(multidim, (as, bs) -> as[0] - bs[0]);
+        for (int dim[] : multidim)
+            System.out.println(Arrays.toString(dim));
 
     }
 }
