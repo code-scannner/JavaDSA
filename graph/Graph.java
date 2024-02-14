a27e6418eb2589ce3b90a945ec134fb38a5e68f6
code-scanner <cyber.gupta001@gmail.com>
2024-02-14 14:51:14 +0530

    array to adjmatrix

--- graph/Graph.java
+++ graph/Graph.java
@@ -13,5 +13,17 @@ public class Graph {
         }
         return result;
     }
+    public static List<List<Integer>> arrayToAdjMatrix(int[][] edges, int V) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < V; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j : edges[i]){
+                list.add(j);
+            }
+            result.add(list);
+        }
+
+        return result;
+    }
 
 }
