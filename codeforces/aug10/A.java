231bf70e754cfbeace91048c5b1de9f651468b73
code-scanner <cyber.gupta001@gmail.com>
2024-08-10 22:02:50 +0530

    aug 10 cf

---
+++ codeforces/aug10/A.java
@@ -0,0 +1,79 @@
+package codeforces.aug10;
+
+import java.util.*;
+import java.io.*;
+
+public class A {
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int xc = sc.nextInt(), yc = sc.nextInt();
+            int k = sc.nextInt();
+            if (k % 2 == 1) {
+                out.println(xc + " " + yc);
+            }
+            for (int i = 0; i < k / 2; i++) {
+                out.println((xc - i - 1) + " " + yc);
+                out.println((xc + i + 1) + " " + yc);
+            }
+
+        }
+
+        out.close();
+    }
+
+    static class Scanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        Scanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        Scanner(String fileName) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(fileName));
+        }
+
+        int[] narr(int n) throws IOException {
+            int result[] = new int[n];
+            for (int i = 0; i < n; i++)
+                result[i] = nextInt();
+            return result;
+        }
+
+        String[] nstr(int n) throws IOException {
+            String result[] = new String[n];
+            for (int i = 0; i < n; i++)
+                result[i] = next();
+            return result;
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws NumberFormatException, IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws NumberFormatException, IOException {
+            return Double.parseDouble(next());
+        }
+
+        boolean ready() throws IOException {
+            return br.ready();
+        }
+    }
+}
---
+++ codeforces/aug10/B.java
@@ -0,0 +1,76 @@
+package codeforces.aug10;
+
+import java.util.*;
+import java.io.*;
+
+public class B {
+    public static void main(String[] args) throws IOException {
+        PrintWriter out = new PrintWriter(System.out);
+        Scanner sc = new Scanner();
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int arr[] = sc.narr(n);
+            for (int i = 1; i < n; i++) {
+                out.print(arr[i] + " ");
+            }
+            out.println(arr[0]);
+
+        }
+
+        out.close();
+    }
+
+    static class Scanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        Scanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        Scanner(String fileName) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(fileName));
+        }
+
+        int[] narr(int n) throws IOException {
+            int result[] = new int[n];
+            for (int i = 0; i < n; i++)
+                result[i] = nextInt();
+            return result;
+        }
+
+        String[] nstr(int n) throws IOException {
+            String result[] = new String[n];
+            for (int i = 0; i < n; i++)
+                result[i] = next();
+            return result;
+        }
+
+        String next() throws IOException {
+            while (st == null || !st.hasMoreTokens())
+                st = new StringTokenizer(br.readLine());
+            return st.nextToken();
+        }
+
+        String nextLine() throws IOException {
+            return br.readLine();
+        }
+
+        int nextInt() throws IOException {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() throws NumberFormatException, IOException {
+            return Long.parseLong(next());
+        }
+
+        double nextDouble() throws NumberFormatException, IOException {
+            return Double.parseDouble(next());
+        }
+
+        boolean ready() throws IOException {
+            return br.ready();
+        }
+    }
+}
